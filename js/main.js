120d8b241b79b214b65783db9598b3279c34e1f0
MAX3 <max-3@users.noreply.github.com>
2017-11-18 13:29:39 +0300

    Start

---
+++ js/main.js
@@ -0,0 +1,316 @@
+
+var tool; //инструмент
+//защита цвета
+//стирание при повторном нажатии
+//прямые линии
+//лупа
+//ладонь(аналог фотошопа)
+//рисование заметок
+var sample; //текущий образец
+
+var holstW = 15; //ширина в клетках
+var holstH = 15; //высота в клетках 
+var boxSize = 20; //размер клетки
+
+//TileEditor
+var stageEditor = new Konva.Stage({
+  container: 'container',
+  width: holstW * boxSize,
+  height: holstH * boxSize
+});
+
+var layerTiles = new Konva.Layer({
+  // x: 20,
+  // y: 20,
+  // draggable: true,
+  // opacity: 0.1
+});
+
+
+
+//     var imageObj = new Image();
+//     imageObj.onload = function() {
+//       var yoda = new Konva.Image({
+//         x: 100,
+//         y: 100,
+//         image: imageObj,
+//         width: 100,
+//         height: 100,
+//         opacity: 1.7,
+//         stroke: 'red',
+//         strokeWidth: 4,
+//         // rotation:45,
+//         offsetX:60,
+//         offsetY:60,
+
+//         // scaleX:2.3,
+//         // scaleY:3.3,
+
+
+//         draggable: true
+//       });
+//       // add the shape to the layer
+//       layer2.add(yoda);
+//       // layer2.scale(10,10);
+//       // layer2.rotation(45);
+//       // yoda.rotation(45);
+//       // yoda.matrix3d(0.99,-0.14,0.00,.001,0.14,0.99,0.00,0.004,0,0,1,0,0,22,1,1);
+
+
+//        // layer2.transform.matrix3d(0.99,-0.14,0.00,.001,0.14,0.99,0.00,0.004,0,0,1,0,0,22,1,1);
+//       // yoda.scaleX(2);
+//       // yoda.skewX(0.4);
+//       // yoda.skewY(0.4);
+//       // yoda.multiply(1,3,3,3,3,3,3,3,3,3);
+//       console.error(
+//         layer2
+//                    );
+//       // add the layer to the stage
+//       stage.add(layer2);
+//     };
+//     imageObj.src = 'https://konvajs.github.io/assets/yoda.jpg';
+
+
+
+
+
+
+
+/////////////////////////////
+var box;
+// generate boxes
+//for (var ix = 0; ix < width / BOX_SIZE; ix++) {// noprotect
+//for (var iy = 0; iy < height / BOX_SIZE; iy++) {// noprotect
+//  for (var iy = 0; iy < height / BOX_SIZE; iy++) {// noprotect
+//        for (var ix = 0; ix < width / BOX_SIZE; ix++) {// noprotect 
+for (var iy = 0; iy < holstH; iy++) { // noprotect
+  for (var ix = 0; ix < holstW; ix++) { // noprotect 
+
+    box = new Konva.Rect({
+      //https://yzorrykodelie.ru/%F0%9F%8C%90/%D0%BB%D0%B8%D1%86%D0%B5%D0%B2%D0%B0%D1%8F.png
+      //https://yzorrykodelie.ru/🌐/лицевая.png
+      x: ix * boxSize,
+      y: iy * boxSize,
+      width: boxSize,
+      height: boxSize,
+      
+      // x : ix * boxSize,
+      // y : iy * boxSize,
+      // width : boxSize - 1,
+      // height : boxSize - 1,
+      // fill : 'darkgrey',
+      stroke: 'gold',
+      // scaleX:1.1,
+      // scaleY:1.1,
+
+
+      strokeWidth: 2
+    });
+    layerTiles.add(box);
+  }
+}
+layerTiles.draw();
+// as all boxes stay separately with no overlap
+// and they have no opacity
+// we can call 'box.draw()' and we will have expected result
+// REMEMBER that is this case box will be drawn on top of existing layer
+// without clearing
+
+stageEditor.add(layerTiles);
+
+//наведение на боксы
+// layer.on('mouseover', function(evt) {
+  layerTiles.on('mousedown mouseover', function (evt) {
+
+  // layer.on('mousemove', function(evt) {
+
+  if (evt.type == "mousedown" || mouseL) { //если мышь нажата
+    var box = evt.target;
+    // console.log(box);
+    box.fill(sample);
+    // layer.draw();
+    // box.draw();
+
+    layerTiles.batchDraw();
+  }
+  // console.log(evt);
+  /*mouseDown = false;
+  // Зажата клавиша мыши
+  window.onmousedown = function(){
+        mouseDown = true;
+  };
+          
+  // Отпущена клавиша мыши
+  window.onmouseup = function(){
+        mouseDown = false;
+  };
+
+  if (mouseDown){
+    console.log("зажата");
+  }else{
+    console.log("Отпущна");    
+  }*/
+
+
+});
+// layer.on('mouseout', function(evt) {
+//     var box = evt.target;
+//     box.fill('darkgrey');
+//     box.draw();
+// });
+function fromIn2(x, y, x2, y2, value) {
+  var prevPos = {
+    x: x2,
+    y: y2
+  };
+  var dx = Math.abs(x - prevPos.x); //смещение относительно предыдущего клика
+  var dy = Math.abs(y - prevPos.y); //смещение относительно предыдущего клика
+  //////////////////////////  
+  var pos = {
+    x: prevPos.x,
+    y: prevPos.y
+  }; //текущая позиция
+  var dist = Math.sqrt(dx * dx + dy * dy); //путь(√)
+  while (prevPos.x != x || prevPos.y != y) {
+    //x,y → конечная позиция
+    //pos → обновляемая позиция текущего шага(десятичная дробь)
+    //dx,dy → смещение новой точки относительно старой
+    //dist→корень из произведения и сложения
+    pos.x += (x > pos.x ? 1 : -1) * dx / dist;
+    pos.y += (y > pos.y ? 1 : -1) * dy / dist;
+    //обновление предудущей позиции
+    prevPos = {
+      x: Math.round(pos.x),
+      y: Math.round(pos.y)
+    };
+    draw(prevPos.x, prevPos.y, value);
+    //DrawSquare(g_prevPos.x*SqWidth, g_prevPos.y*SqWidth, g_prevPos.x, g_prevPos.y);
+    // console.error(prevPos);
+  }
+  // draw2(x,y,"red");
+  // draw2(x2,y2,"red");
+}
+
+function fromIn(x, y, x2, y2, value) {
+
+  var razn1 = Math.abs(x - x2) + 1;
+  var razn2 = Math.abs(y - y2) + 1;
+  console.warn(razn1, razn2);
+  var pathSplit;
+  var stepX, stepY;
+  if (razn1 > razn2) { // }else if(razn1<razn2){
+    pathSplit = razn1;
+    stepX = 1;
+    stepY = 1;
+  } else {
+    stepX = (razn1 - 1) / razn2;
+    stepY = 1;
+    pathSplit = razn2;
+  }
+  console.error(razn1 + ":" + razn2, pathSplit, stepX, stepY);
+  var posX = x + stepX;
+  var posY = y;
+  for (var n = 0; n < pathSplit; n++) {
+    console.warn(n + 1 + "|", Math.round(posX), Math.round(posY), "|", posX + ":" + posY);
+    draw(Math.round(posX), Math.round(posY));
+    // draw(posX.toFixed(),posY.toFixed());
+
+    //n.toFixed()
+    posX = posX + stepX;
+    posY = posY + stepY;
+    draw2(x, y, "black");
+    draw2(x2, y2, "black");
+  }
+
+}
+
+// fromIn(5,2,1,2,"red");
+// fromIn(2,1,3, 8,"red");
+// fromIn(5,1,7, 8,"red");
+// fromIn(8,1,11,8,"red");
+// fromIn2(1,1,10, 10,"blue");
+// fromIn2(2,9,3, 16,"blue");
+// fromIn2(5,9,7, 16,"blue");
+// fromIn2(8,9,11,16,"blue");
+
+
+fromIn2(1, 1, 10, 9, "blue");
+fromIn2(2, 3, 9, 3, "#FDA226");
+fromIn2(8, 2, 8, 5, "#FDA226");
+fromIn2(3, 7, 7, 7, "#EB4078");
+
+
+
+function draw(x, y, value) {
+  if (!value) value = "red"; //если не передано значения то красный
+  if (x > holstW) { //отсев ошибок  
+    console.error("oops W");
+    return;
+  } else if (y > holstH) {
+    console.error("oops H");
+    return;
+  }
+  var n = holstW * (y - 1) + x - 1;
+  var box = layerTiles.children[n];
+  // console.log(box); 
+  box.fill(value);
+  box.draw();
+}
+
+function draw2(x, y, value) {
+  if (!value) value = "red"; //если не передано значения то красный
+  if (x > holstW) { //отсев ошибок  
+    console.error("oops W");
+    return;
+  } else if (y > holstH) {
+    console.error("oops H");
+    return;
+  }
+  var n = holstW * (y - 1) + x - 1;
+  var box = layerTiles.children[n];
+  // console.log(box);
+  box.stroke(value);
+  box.draw();
+
+
+
+
+
+
+
+}
+// draw(3,3);
+
+
+
+// 	g_sq.mousedown(function(ev)
+// 	{
+// 		Draw(ev, true);
+// 		g_down = true;
+// 	});
+
+
+
+// document.onclick = function(e) {
+//   // if (!e.altKey || !e.shiftKey) return;
+//   // alert( 'Ура!' );
+//   console.log("клик");
+// };
+var mouseL; //флаг о нажатии мыши
+$(document).mousedown(function (e) {
+  if (e.which == 1) {
+    mouseL = true;
+  }
+});
+$(document).mouseup(function (e) {
+  if (e.which == 1) {
+    console.log("отжал");
+    mouseL = false;
+  }
+});
+$(".samples").click(function () {
+  $(".act").removeClass("act");
+  $(this).addClass("act");
+  sample = $(this).css("background-color");
+});
+$('#sample15').trigger('click');
---
+++ js/sandbox.js
@@ -0,0 +1,324 @@
+console.clear();
+console.log("я родился");
+
+
+/**/
+var boxSize = 20; //размер клетки
+
+
+
+//создаём сцену
+var stage = new Konva.Stage({
+  container: 'container',
+  width: 500,
+  height: 700,
+});
+
+
+
+var layer = new Konva.Layer();
+//векторная фигура
+var tilePath = new Konva.Path({
+  //8 9 10 12 bad 
+  data: s1,
+  fill: "red",
+  x: 0,
+  y: 0,
+  scale: {
+    x: boxSize / 20,
+    y: boxSize / 20
+  }
+});
+//цвет выбранного узора
+var tileBg = new Konva.Rect({
+  stroke: 'black',
+  strokeWidth: 2,
+  x: -1,
+  y: -1,
+  width: boxSize + 2,
+  height: boxSize + 2,
+  fill: "gold",
+  // opacity:0.8,
+});
+var tile = new Konva.Group({
+  x: 5,
+  y: 5,
+  width: boxSize,
+  height: boxSize,
+  // scale: {
+  // x : 2,
+  // y : 2
+  // }
+});
+tile.add(tileBg, tilePath);
+var bigClone = tile.clone({
+  x: 40,
+  y: 40,
+  scale: {
+    x: 5,
+    y: 5
+  }
+});
+
+var box00 = new Konva.Rect({
+  stroke: 'black',
+  strokeWidth: 2,
+  x: 40,
+  y: 310,
+  width: tile.width(),
+  height: tile.height(),
+  // fill:"green",
+  // fillPatternImage:img,
+  // fillPatternRepeat:'repeat-x',
+  fillPatternRepeat: 'no-repeat',
+  opacity: 0.8,
+});
+//создание изображения из тайла
+var image = tile.toImage({
+  x: tile.x(),
+  y: tile.y(),
+  width: tile.width(),
+  height: tile.height(),
+  callback: function (img) {
+    qwe1(img);
+    box00.fillPatternImage(img);
+    box01.fillPriority('pattern');
+    box01.fillPatternImage(img);
+    layer.draw();
+    // box00.draw();
+    // box01.draw();
+
+    // do stuff with img
+    // console.warn(img);
+    // box1.fillPatternImage(img);
+    // box1.draw();
+    // return img;
+  }
+});
+
+function qwe1(img) {
+  var box1 = new Konva.Rect({
+    stroke: 'black',
+    strokeWidth: 2,
+    x: 10,
+    y: 100,
+    width: 400,
+    height: 200,
+    // fill: "gold",
+    fillPatternImage: img,
+    // fillPatternRepeat:'repeat-x',
+    // fillPatternRepeat:'no-repeat',
+    opacity: 0.8,
+
+  });
+  var box2 = new Konva.Rect({
+    stroke: 'black',
+    strokeWidth: 2,
+    x: 10,
+    y: 310,
+    width: tile.width(),
+    height: tile.height(),
+    fillPatternImage: img,
+    // fillPatternRepeat:'repeat-x',
+    fillPatternRepeat: 'no-repeat',
+    opacity: 0.8,
+  });
+  layer.add(box1, box2);
+  // layer.draw();
+}
+var box01 = new Konva.Rect({
+  stroke: 'black',
+  strokeWidth: 2,
+  x: 70,
+  y: 310,
+  width: tile.width(),
+  height: tile.height(),
+  fill: "green",
+  // fillPatternImage:img,
+  // fillPatternRepeat:'repeat-x',
+  fillPatternRepeat: 'no-repeat',
+  opacity: 0.8,
+});
+layer.add(bigClone);
+layer.add(tile);
+layer.add(box00);
+layer.add(box01);
+
+stage.add(layer);
+// layer.draw();
+
+
+//     var rect = new Konva.Rect({
+//       // stroke: 'blue',
+//       // strokeWidth: null,
+//       width: 20,
+//       height: 20,
+//       fill: "red",
+//       opacity:0.4,
+
+//     }); 
+// var bug = new Konva.Rect({
+//       stroke: 'blue',
+//       strokeWidth: 4,
+//      fill:"green",
+//       width: 100,
+//       height: 100,
+//       opacity:0.5,
+//       x: 5,
+//       y: 5,
+//   draggable: true,
+//     });
+// var tile = new Konva.Group({
+//    draggable: true,
+//         x: 10,
+//         y: 10,
+//         scale: {
+//           x : 10,
+//           y : 10
+//         }
+//     });
+// tile.add(rect);
+// tile.add(path);
+// layer.add(tile);
+// tile.cache();
+//     var clone = tile.clone({
+//         x: -10,
+//         y: -10,
+//               scale: {
+//           x : 1,
+//           y : 1
+//         }
+
+//       });
+//     // clone.cache();
+// layer.add(clone);
+// console.log(clone.cache());
+// console.log(tile); 
+
+// console.error(imageTest);
+// var imageTest;
+// var image = clone.toImage({
+//   x: -10,
+//   y: -10,
+//   width: 20,
+//   height: 20,
+//   callback: function(img) {
+//     // do stuff with img
+//     console.error(img);
+//     imageTest = img;
+//   }
+// });
+
+
+
+
+
+//https://yzorrykodelie.ru/🌐/лицевая.png
+// var yoda2 = new Konva.Image({
+//         x: 33,
+//         y: 33,
+//         fill: "gold",
+//         // image: imageObj,
+//         stroke: 'red',
+//       strokeWidth: 32,
+//         width: 233,
+//         height: 233
+//       });
+//  var imageObj = new Image();
+// // console.error(imageObj);
+//  var imageObj2 = new Image();
+// // console.error(imageObj2);
+//     imageObj.onload = function() {
+
+//       var yoda = new Konva.Image({
+//         x: 50,
+//         y: 50,
+//         image: imageObj,
+//         width: 20,
+//         height: 20
+//       });
+// // layer.draw();
+//       // add the shape to the layer
+//       layer.add(yoda);
+
+//       // add the layer to the stage
+//     var rect2 = new Konva.Rect({
+//       stroke: 'red',
+//       strokeWidth: 2,
+//       x:10,y:224,
+//       width: 60,
+//       height: 60,
+//       // fill: "red",
+//       fillPatternImage:imageTest,
+//       // opacity:0.8,
+//     });
+//     rect2.fillPatternRepeat('no-repeat');
+//     console.error(imageTest);
+//     var rect3 = new Konva.Rect({
+//       stroke: 'black',
+//       strokeWidth: 2,
+//       x:10,y:300,
+//       width: 220,
+//       height: 100,
+//       // fill: "red",
+//       fillPatternImage:imageTest,
+//       // fillPatternRepeat:'repeat-x',
+//       // fillPatternRepeat:'no-repeat',
+//       // opacity:0.8,
+//     });
+//       var yod = new Konva.Image({
+//         x: 230,
+//         y: 40,
+//         // fill:"green",
+//         image: imageTest,
+//         width: 100,
+//         height: 100,
+//         stroke: 'red',
+//         strokeWidth: 2,
+//       });
+//        var yod2 = new Konva.Image({
+//         x: 230,
+//         y: 150,
+//         fill:"green",
+//         image: imageTest,
+//         // width: 100,
+//         // height: 100,
+//         stroke: 'red',
+//         strokeWidth: 2,
+//       });
+
+
+
+
+
+// console.warn(imageObj);
+// console.warn(yoda.attrs.image.outerHTML);
+//       var fillPatternImage = rect2.fillPatternImage();
+// console.warn(yoda);
+// console.log(fillPatternImage);
+// // layer.add(yoda);
+//     };
+//     imageObj.src = 'https://yzorrykodelie.ru/🌐/лицевая.png';
+
+
+
+
+
+/*
+// get crop
+var crop = image.crop();
+
+// set crop
+image.crop({
+  x: 20,
+  y: 20,
+  width: 20,
+  height: 20
+});*/
+
+
+
+
+
+
+// add the layer to the stage
