379647d76e7750133ea7e94d26046f1887faab78
MAX3 <max-3@users.noreply.github.com>
2017-12-17 20:21:11 +0300

    [key ctrl] → pipette

--- js/pen.js
+++ js/pen.js
@@ -16,19 +16,33 @@ for (var i = 0, l = holstH; i < l; i++) {
 
 // console.warn(BD1);
 
+// layer.on('mousemove', function(evt) {
+//КликКлак
 layerTiles.on('mousedown mouseover', function (evt) {
-
-  // layer.on('mousemove', function(evt) {
-
-  // layerTiles.opacity(1);
-  // layerTiles.draw();
-  //КликКлак
+  var box = evt.target; 
+  var X = box.x() / boxSize;
+  var Y = box.y() / boxSize;
   if (evt.type == "mousedown" || mouseL) { //если мышь нажата
     if (keySpace) { //если нажат пробел
       // layerTiles.draggable(true);//включить перетаскивание
-      return false;//не продолжать
+      return false; //не продолжать
+    } else if (evt.evt.ctrlKey) { //если нажат контрол
+      sample = BD1[Y][X][0];
+      color = BD1[Y][X][1];
+      console.log('%c%s', 'color: gold;', evt.evt.ctrlKey);
+      if (sample) { TE.selected.sample = sample}
+      if (color) { TE.selected.color = color }
+      selectSample();
+      console.log('%c%s', 'color: red;', BD1[Y][X][0]);
+      console.log('%c%s', 'color: red;', BD1[Y][X][1]);
+      
+
+      return false; //не продолжать
     }
-    var box = evt.target;
+    // console.log(evt);
+
+
+
     box.name(TE.selected.sample);
     // console.log(box);
     // box.fill(sample);
@@ -42,33 +56,14 @@ layerTiles.on('mousedown mouseover', function (evt) {
     // addLayer(TE.selected.sample, testIMG);
 
 
-    var X = box.x() / boxSize;
-    var Y = box.y() / boxSize;
+  // var X = box.x() / boxSize;    
+  // var Y = box.y() / boxSize;
     BD1[Y][X][0] = TE.selected.sample;
     BD1[Y][X][1] = TE.selected.color;
     // console.log(X, Y, BD1[Y][X]);
 
 
   }
-  // console.log(evt);
-  /*mouseDown = false;
-  // Зажата клавиша мыши
-  window.onmousedown = function(){
-        mouseDown = true;
-  };
-          
-  // Отпущена клавиша мыши
-  window.onmouseup = function(){
-        mouseDown = false;
-  };
-
-  if (mouseDown){
-    console.log("зажата");
-  }else{
-    console.log("Отпущна");    
-  }*/
-
-
 });
 
 
@@ -89,26 +84,25 @@ $(document).mouseup(function (e) { //отжал мышь
 
 var keySpace; //флаг о нажатии мыши
 $(document).keydown(function (e) { //нажал клаву
-  e.preventDefault(); //отключить действие по умолчанию
   if (keySpace) { //если флаг уже поставлен то не продолжать 
     return false; //#TODO проверить есть ли профит от данной конструкции
   }
-
   if (e.which == 32) { //32-пробел
+    e.preventDefault(); //отключить действие по умолчанию
     console.log('%c%s', 'color: gold;', "↓", e.which);
     keySpace = true; //нажал
-    document.body.style.cursor = 'move';//ставим курсор перетаскивания
+    document.body.style.cursor = 'move'; //ставим курсор перетаскивания
     //#TODO grab, grabbing
-    layerTiles.draggable(true);//разрешаем перетаскивать слой с тайлами
+    layerTiles.draggable(true); //разрешаем перетаскивать слой с тайлами
   }
 });
 
 $(document).keyup(function (e) { //отжал клаву 
   if (e.which == 32) {
-     console.log('%c%s', 'color: green;', "↑", e.which);
+    console.log('%c%s', 'color: green;', "↑", e.which);
     keySpace = false; //отжал 
-    document.body.style.cursor = 'default';//возвращаем курсор
-    layerTiles.draggable(false);//запрещаем перетаскивать слой с тайлами
+    document.body.style.cursor = 'default'; //возвращаем курсор
+    layerTiles.draggable(false); //запрещаем перетаскивать слой с тайлами
   }
 });
 
